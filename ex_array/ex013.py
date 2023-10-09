2b3f549c9dc1d9ef27521bf53b542e587e99ce7d
Gabriel Nogueira <gabriel.nogueira013@gmail.com>
2023-10-09 00:06:50 -0300

    ex013.py

--- ex_array/ex013.py
+++ ex_array/ex013.py
@@ -7,16 +7,13 @@ for x in range(3):
     vetores[x] = int(input(f"digite o {x + 1}º valor: "))
     if x == 0:
         maiorv = menorv = vetores[x]
-
 par = [i for i in vetores if i % 2 == 0]
-
 for i in vetores:
     if i < menorv:
         menorv = i
     if i > maiorv:
         maiorv = i
     soma += i
-
 media = soma / 3
 vacm = sum(1 for j in vetores if j > media)
 
