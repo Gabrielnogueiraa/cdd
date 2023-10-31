c57ce1328001f87712a9a094498acf95a2e4a64a
Gabriel Nogueira <gabriel.nogueira013@gmail.com>
2023-10-31 11:03:47 -0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from biblioteca import Cliente
-cliente = Cliente(numero=611, saldo=4500, nome='João', tipo='Conta Corrente', limite=9000, status='.')
-cliente.ativarconta()
-cliente.depositar(300)
-cliente.sacar(400)
-cliente.verificarsaldo()
