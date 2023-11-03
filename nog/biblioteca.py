445a581758f576730a4b4b53a540de18aad016d9
Gabriel Nogueira <gabriel.nogueira013@gmail.com>
2023-11-03 01:04:12 -0300

    Update biblioteca.py

--- nog/biblioteca.py
+++ nog/biblioteca.py
@@ -6,6 +6,7 @@ class Cliente:
         self.tipo = tipo
         self.status = False
         self.limite = limite
+        
     def depositar(self,valor):
         if self.status == False:
             print(f'{self.nome} não possui uma conta ativa.')
@@ -15,6 +16,22 @@ class Cliente:
             print(f'Seu novo saldo é {self.saldo:.2f}')
         else:
             print('Valor inválido.')
+            
+    def ativarLimite(self, limite):
+        if self.status == False:
+            print(f'Não existe uma conta ativa.')
+        else:
+            self.limite = limite
+            print(f'Limite de {self.limite} foi ativado.')
+
+    def verificarLimite(self):
+        if self.status == False:
+            print(f'Não existe uma conta ativa.')
+        elif self.status and self.limite == 0:
+            print(f'Você não tem limite ativo. ')
+        else:
+            print(f'Você possui limite ativo. Ele é de: R$ {self.limite:.2f}.')
+            
     def sacar(self,valor):
         if self.status == False:
             print(f'{self.nome} não possui uma conta ativa.')
@@ -23,12 +40,14 @@ class Cliente:
             print(f'Você sacou {valor:.2f}, seu novo saldo é de {self.saldo:.2f}')
         else:
             print('Saldo insuficiente.')
+            
     def ativarconta(self):
         if self.status == False:
             print(f'{self.nome}, sua conta foi ativada')
             self.status = True
         else:
             print('Sua conta já está ativada')
+            
     def verificarsaldo(self):
         if self.status == False:
             print(f'{self.nome} não possui uma conta ativa.')
