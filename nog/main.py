5d2be05cc659cae91649e0a8eb43948b58b5232f
Gabriel Nogueira <gabriel.nogueira013@gmail.com>
2023-11-03 01:05:29 -0300

    Update main.py

--- nog/main.py
+++ nog/main.py
@@ -1,6 +1,8 @@
 from biblioteca import Cliente
-cliente = Cliente(numero=611, saldo=4500, nome='João', tipo='Conta Corrente', limite=9000, status='.')
+cliente = Cliente(numero=611, saldo=4500, nome='João', tipo='Conta Corrente', status='.')
 cliente.ativarconta()
+cliente.ativarLimite(1000)
 cliente.depositar(300)
 cliente.sacar(400)
 cliente.verificarsaldo()
+cliente.verificarLimite()
